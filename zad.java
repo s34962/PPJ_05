a900895aa3088f2bffb4c2e50d0a66c2a919ad82
Zooqoo69 <zooqoo69@gmail.com>
2025-11-18 23:33:22 +0100

    Zad VII dodatkowe

--- zad.java
+++ zad.java
@@ -1,40 +1,63 @@
 public class zad{
-	public static void main (String[] args){
+	public static void main(String[] args){
 		java.util.Scanner in = new java.util.Scanner(System.in);
-		IO.println("Enter n: ");
+		IO.println("Enter n:");
 		int n = in.nextInt();
-		int[] tab = new int [n];
-		int[] counTab = new int[5];
-		int minIndex = 0; boolean contain = false;
-	
-	
-	for(int i = 0; i < tab.length; i++){
-		tab[i] = (int)(Math.random() * 10) + 1;
-		IO.print(tab[i] + " ");
-	}
-	IO.println();
-	
-	for(int i = 0; i < tab.length; i++){
-		for(int j = 0; j < counTab.length; j++){
-			if(tab[i] == counTab[j])
-				contain = true;
+		int[] tab = new int[n];
+		int[] tabCount = new int[tab.length];
+		int[] tabSort = new int[tab.length];
+		
+		for(int i = 0; i < tab.length; i++){
+			tab[i] = (int)(Math.random() * 10) + 1;
+			IO.print(tab[i] + " ");
+		}
+		IO.println();
+		
+		int checkNum = 0;
+		
+		for(int i = 0; i < tab.length; i++){
+			for(int j = 0; j < tab.length; j++)
+				if(tab[i] == tab[j])
+					checkNum++;
+			tabCount[i] = checkNum;
+			checkNum = 0;
 		}
 		
-		
-		if(contain == false){
-			if(minIndex >= counTab.length){
-			int[] tmp = new int[counTab.length * 2];
-			for(int k = 0; k < counTab.length; k++)
-				tmp[k] = counTab[k];
-			counTab = tmp;	
+		for(int i = 0; i < tab.length; i++){
+			for(int j = 0; j < tab.length; j++){
+				if(tabCount[i] > tabCount[j]){
+					int tmp = tabCount[j];
+					tabCount[j] = tabCount[i];
+					tabCount[i] = tmp;
+					
+					tmp = tab[j];
+					tab[j] = tab[i];
+					tab[i] = tmp;
+				}
+			}
 		}
-			counTab[minIndex] = tab[i];
-			minIndex++;
-		}
-		contain = false;
-	}
-	for(int i = 0; i < counTab.length - (counTab.length - minIndex); i++){
-		IO.print(counTab[i] + " ");
-	}
+			
+		int minIndex = 0;
+		int sortIndex = 0;
+		boolean exist = false;
 		
-}}
+		for(int i = 0; i < tab.length; i++){
+			for(int j = 0; j < tab.length; j++){	
+				if (tab[i] == tabSort[j])
+					exist = true;		
+				else	
+					continue;
+			}
+			
+		if(exist == false && tabCount[minIndex] == tabCount[i])
+		tabSort[sortIndex++] = tab[i];
+	
+		exist = false;
+		}
+
+		for(int i = 0; i < tab.length - (tab.length - sortIndex); i++)
+			IO.print(tabSort[i] + " ");
+	}
+}
+							
+						
