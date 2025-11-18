4e89c212e992fa09560f3579ebaf46681b6d6866
Zooqoo69 <zooqoo69@gmail.com>
2025-11-18 15:22:42 +0100

    Zad VI dodatkowe

--- zad.java
+++ zad.java
@@ -1,53 +1,40 @@
 public class zad{
 	public static void main (String[] args){
-		int[] A = new int[10];
-		int[] B = new int[A.length];
-		int[] C = new int[8];
-		int ilosC = 0;
-		for(int i = 0; i < A.length; i++){
-			A[i] = (int)(Math.random() * 100) + 1;
-			B[i] = (int)(Math.random() * 100) + 1;
-		}
-		
-		for(int minIndex = 0; minIndex < A.length; minIndex++){
-			for(int i = minIndex; i < A.length; i++){
-				if(A[i] < A[minIndex]){
-					int tmp = A[i];
-					A[i] = A[minIndex];
-					A[minIndex] = tmp;
-				}
-			}
-			IO.print(A[minIndex] + " ");
-		}
-		IO.println();
-		
-		for(int maxIndex = 0; maxIndex < A.length; maxIndex++){
-			for(int i = maxIndex; i < A.length; i++){
-				if(B[i] > B[maxIndex]){
-					int tmp = B[i];
-					B[i] = B[maxIndex];
-					B[maxIndex] = tmp;
-				}
-			}
-			IO.print(B[maxIndex] + " ");
-		}
-		IO.println();
-		
-		for(int i = 0; i < A.length + B.length; i++){
-			if(i >= C.length-1){
-				int[] tmp = new int[C.length * 2];
-				for(int j = 0; j < C.length; j++)
-					tmp[j] = C[j];
-				C = tmp;
-			}
-			if(i < A.length){
-			C[i] = A[i]; ilosC++;}
-			
-			else{
-			C[i] = B[i - A.length]; ilosC++;}
-		}
-		for(int i = 0; i < C.length - (C.length - ilosC); i++)
-			IO.print(C[i] + " ");
-		
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		IO.println("Enter n: ");
+		int n = in.nextInt();
+		int[] tab = new int [n];
+		int[] counTab = new int[5];
+		int minIndex = 0; boolean contain = false;
+	
+	
+	for(int i = 0; i < tab.length; i++){
+		tab[i] = (int)(Math.random() * 10) + 1;
+		IO.print(tab[i] + " ");
 	}
-}
+	IO.println();
+	
+	for(int i = 0; i < tab.length; i++){
+		for(int j = 0; j < counTab.length; j++){
+			if(tab[i] == counTab[j])
+				contain = true;
+		}
+		
+		
+		if(contain == false){
+			if(minIndex >= counTab.length){
+			int[] tmp = new int[counTab.length * 2];
+			for(int k = 0; k < counTab.length; k++)
+				tmp[k] = counTab[k];
+			counTab = tmp;	
+		}
+			counTab[minIndex] = tab[i];
+			minIndex++;
+		}
+		contain = false;
+	}
+	for(int i = 0; i < counTab.length - (counTab.length - minIndex); i++){
+		IO.print(counTab[i] + " ");
+	}
+		
+}}
