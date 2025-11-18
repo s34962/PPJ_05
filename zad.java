bfe54060923780af226e9541a9f20fa7cdc6cf20
Zooqoo69 <zooqoo69@gmail.com>
2025-11-18 12:40:34 +0100

    zadV dodatkowe

--- zad.java
+++ zad.java
@@ -1,42 +1,53 @@
 public class zad{
-	public static void main(String[] args){
-	int[]x = new int[10];
-	int[] y = new int[x.length];
-	
-	IO.println("x: ");
-	for(int i = 0; i < x.length; i++){
-		x[i] = (int)(Math.random() * 20) - 10;
-		IO.print(x[i] + " ");	
-	}
-	IO.println();
-	
-	IO.println("y: ");
-		for(int i = 0; i < x.length; i++){		
-		y[i] = (int)(Math.random() * 20) - 10;
-		IO.print( y[i] + " ");
-	}
-	IO.println();
-	
-	for(int minIndex = 0; minIndex < x.length; minIndex++){
-		for(int i = minIndex; i < x.length; i++){
-			if(x[i] < x[minIndex]){
-				int tmp = x[minIndex];
-				x[minIndex] = x[i];
-				x[i] = tmp;}
-			if(y[i] < y[minIndex]){
-				int tmp = y[minIndex];
-				y[minIndex] = y[i];
-				y[i] = tmp;
+	public static void main (String[] args){
+		int[] A = new int[10];
+		int[] B = new int[A.length];
+		int[] C = new int[8];
+		int ilosC = 0;
+		for(int i = 0; i < A.length; i++){
+			A[i] = (int)(Math.random() * 100) + 1;
+			B[i] = (int)(Math.random() * 100) + 1;
+		}
+		
+		for(int minIndex = 0; minIndex < A.length; minIndex++){
+			for(int i = minIndex; i < A.length; i++){
+				if(A[i] < A[minIndex]){
+					int tmp = A[i];
+					A[i] = A[minIndex];
+					A[minIndex] = tmp;
+				}
 			}
-		}	
-	}		
-	int miX = x[0], maX = x[x.length - 1];
-	
-	int miY = y[0], maY = y[x.length - 1];
-	
-	int szer = maX - miX;
-	int wys = maY - miY;
-	
-	IO.println("szerokość: " + szer + " wysokość: " + wys + " środek x: " + (((double)(szer))/2) + " y: " + (((double)(wys))/2)); 
+			IO.print(A[minIndex] + " ");
+		}
+		IO.println();
+		
+		for(int maxIndex = 0; maxIndex < A.length; maxIndex++){
+			for(int i = maxIndex; i < A.length; i++){
+				if(B[i] > B[maxIndex]){
+					int tmp = B[i];
+					B[i] = B[maxIndex];
+					B[maxIndex] = tmp;
+				}
+			}
+			IO.print(B[maxIndex] + " ");
+		}
+		IO.println();
+		
+		for(int i = 0; i < A.length + B.length; i++){
+			if(i >= C.length-1){
+				int[] tmp = new int[C.length * 2];
+				for(int j = 0; j < C.length; j++)
+					tmp[j] = C[j];
+				C = tmp;
+			}
+			if(i < A.length){
+			C[i] = A[i]; ilosC++;}
+			
+			else{
+			C[i] = B[i - A.length]; ilosC++;}
+		}
+		for(int i = 0; i < C.length - (C.length - ilosC); i++)
+			IO.print(C[i] + " ");
+		
 	}
-}	
+}
